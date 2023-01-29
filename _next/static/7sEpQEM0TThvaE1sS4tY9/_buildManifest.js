4f1f50fe5e4510ba64c82d83825c031732e468ee
waynechoidev <66890884+waynechoidev@users.noreply.github.com>
2023-01-29 09:37:59 +0000

    Deploying to gh-pages from @ waynechoidev/turing-machine@142a9d9c41949b3ec4cc7e510f092d8ba4a01ff5 🚀

---
+++ _next/static/7sEpQEM0TThvaE1sS4tY9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fa0e700ac002ecdb.css","static/chunks/pages/index-1e5a7bd38ca7649c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
