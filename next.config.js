c4e0ede2dc5b1476a4259256fab83b03f5e055e1
radicalDilettante <wonjun92@gmail.com>
2023-01-29 22:41:12 +1300

    fix: prefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
+  assetPrefix: process.env.NODE_ENV === "production" ? "/turing-machine" : "",
 };
 
 module.exports = nextConfig;
