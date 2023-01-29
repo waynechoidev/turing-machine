b6918680e2a14f90b2e0ab9f5096663d02875ded
radicalDilettante <wonjun92@gmail.com>
2023-01-29 22:50:37 +1300

    fix: prefix

--- components/processor.tsx
+++ components/processor.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./processor.module.css";
+import { assetPrefix } from "../next.config";
 import { TuringMachine } from "../service/example";
 
 interface IProps {
@@ -11,7 +12,12 @@ export default function Processor({ machine, state }: IProps) {
   return (
     <section className={styles.container}>
       <div className={styles.arrow}>
-        <img src="/up.svg" width="50px" height="50px" alt="arrow" />
+        <img
+          src={`${assetPrefix}/up.svg`}
+          width="50px"
+          height="50px"
+          alt="arrow"
+        />
       </div>
       <div className={styles.processor}>
         <h2>Machine States</h2>
